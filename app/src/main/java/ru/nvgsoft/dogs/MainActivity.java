d6dacce64c879ac53b0344389016e52e662c355a
Daniil Shcherbakov <Charel@bk.ru>
2024-04-26 11:02:07 +0300

    Create project, create class DogImage, MainActivity, MainViewModel.

---
+++ app/src/main/java/ru/nvgsoft/dogs/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.nvgsoft.dogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+ private MainViewModel viewModel;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        viewModel.loadDogImage();
+        viewModel.getDogImage().observe(this, new Observer<DogImage>() {
+            @Override
+            public void onChanged(DogImage dogImage) {
+                Log.d(TAG, dogImage.toString());
+            }
+        });
+    }
+
+
+}
